50db2e925467989cd5bad021be948c12a8feb567
Luo Freda <freda.luo@gmail.com>
2024-07-21 17:44:01 -0400

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4173,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
   },
   build: {
     outDir: 'dist',
