91f54a6ee54965f725d5ba44618d26722a3c510f
Luo Freda <freda.luo@gmail.com>
2024-08-08 01:51:51 -0400

    uodates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
-  },
+
   build: {
     outDir: 'dist',
   },
